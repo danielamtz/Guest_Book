df63685cb67a6388af8d05550e03199aedfc5df6
danielamtz <34047204+danielamtz@users.noreply.github.com>
2018-05-31 12:20:22 -0500

    s

--- zmbies/app.js
+++ zmbies/app.js
@@ -4,7 +4,6 @@ var express= require('express');
 var logger = require ('morgan');
 var bodyParser =require('body-parser');
 
-
 var app= express();
 
 app.set('views',path.resolve(__dirname, 'views'));
